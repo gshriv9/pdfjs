1e34be9b4c5b25d7c6c817f622cb4004a698c79c
gshriv9 <gshrivastava27@gmail.com>
2020-12-03 15:38:15 -0500

    intitial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# PDF.js Express - Angular sample
+
+[PDF.js Express](https://pdfjs.express/) is a powerful JavaScript-based PDF Library that leverages PDF.js and adds additional features such as annotations, form support, and digitial signatures. It provides a slick out-of-the-box responsive UI that interacts with the core library to view, annotate and manipulate PDFs that can be embedded into any web project.
+
+This repo is specifically designed for any users interested in integrating WebViewer into Angular project. This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.3. See [Angular - Getting Started](https://angular.io/guide/quickstart) for more information.
+
+## Initial setup
+
+Before you begin, make sure your development environment includes [Node.js](https://nodejs.org/en/).
+
+## Install
+
+```
+git clone https://github.com/pdfjs-express/pdfjs-express-angular-sample.git
+cd pdfjs-express-angular-sample
+npm install
+```
+
+## Run
+
+```
+npm start
+```
+
+Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `npm run build` to build the development build of the project.
+Run `npm run build-prod` to build the production build of the project.
+The build artifacts will be stored in the `dist/` directory.
+
+To run the build, navigate to the `dist/` directory, and run `http-server`. Navigate to the url: host:port/index.html to see the app
+(https://github.com/http-party/http-server/issues/525).
+
+
+## WebViewer APIs
+
+See [API documentation](https://pdfjs.express/documentation).
+
+## Contributing
+
+See [contributing](./CONTRIBUTING.md).
+
+## License
+
+See [license](./LICENSE).
---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component, ViewChild, OnInit, ElementRef, AfterViewInit } from '@angular/core';
+import WebViewer from '@pdftron/pdfjs-express';
+import ExpressUtils from '@pdftron/pdfjs-express-utils';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('viewer', { static: false }) viewer: ElementRef;
+  wvInstance: any;
+
+  ngAfterViewInit(): void {
+
+    WebViewer({
+      path: '../lib',
+      initialDoc: '../files/fw9.pdf'
+    }, this.viewer.nativeElement).then(instance => {
+      this.wvInstance = instance;
+      const utils = new ExpressUtils();
+      const { docViewer, annotManager } = instance;
+
+      // now you can access APIs through this.webviewer.getInstance()
+      // instance.openElements(['notesPanel']);
+      // see https://www.pdftron.com/documentation/web/guides/ui/apis for the full list of APIs
+
+      // or listen to events from the viewer element
+      this.viewer.nativeElement.addEventListener('pageChanged', (e) => {
+        const [pageNumber] = e.detail;
+        console.log(`Current page is ${pageNumber}`);
+      });
+
+      instance.setHeaderItems((header) => {
+        header.push({
+          type: 'actionButton',
+          img: '',
+          onClick: async () => {
+            const xfdf = await annotManager.exportAnnotations({ links: false, widgets: false });
+            const fileData = await docViewer.getDocument().getFileData({});
+            const resp = await utils.setFile(fileData).setXFDF(xfdf).merge();
+            const mergedBlob = await resp.getBlob();
+            // saveAs(mergedBlob, 'myfile.pdf')
+            await resp.deleteFile();
+          }
+        });
+      });
+
+      // or from the docViewer instance
+      instance.docViewer.on('annotationsLoaded', () => {
+        console.log('annotations loaded');
+      });
+
+      instance.docViewer.on('documentLoaded', this.wvDocumentLoadedHandler)
+    });
+  }
+
+  ngOnInit() {
+    // const utils = new ExpressUtils();
+    this.wvDocumentLoadedHandler = this.wvDocumentLoadedHandler.bind(this);
+    // utils.setFile('../files/fw9.pdf');
+    // const response = await utils.extract(); // extract XFDF
+    // const { xfdf } = response;
+  }
+
+  wvDocumentLoadedHandler(): void {
+    // you can access docViewer object for low-level APIs
+    const docViewer = this.wvInstance;
+    const annotManager = this.wvInstance.annotManager;
+    // and access classes defined in the WebViewer iframe
+    const { Annotations } = this.wvInstance;
+    const rectangle = new Annotations.RectangleAnnotation();
+    rectangle.PageNumber = 1;
+    rectangle.X = 100;
+    rectangle.Y = 100;
+    rectangle.Width = 250;
+    rectangle.Height = 250;
+    rectangle.StrokeThickness = 5;
+    rectangle.Author = annotManager.getCurrentUser();
+    annotManager.addAnnotation(rectangle);
+    annotManager.drawAnnotations(rectangle.PageNumber);
+    // see https://www.pdftron.com/api/web/WebViewer.html for the full list of low-level APIs
+  }
+}
