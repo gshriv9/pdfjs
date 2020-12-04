0b3d10a8db358434f99adfcca45cb26f42926dc2
gshriv9 <gshrivastava27@gmail.com>
2020-12-04 18:34:05 -0500

    Latest Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild, OnInit, ElementRef, AfterViewInit } from '@angula
 import WebViewer from '@pdftron/pdfjs-express';
 import ExpressUtils from '@pdftron/pdfjs-express-utils';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,15 +16,21 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     WebViewer({
       path: '../lib',
-      initialDoc: '../files/fw9.pdf'
+      initialDoc: '../files/sample.pdf'
     }, this.viewer.nativeElement).then(instance => {
       this.wvInstance = instance;
       const utils = new ExpressUtils();
       const { docViewer, annotManager } = instance;
-
+      docViewer.on('documentLoaded', () => {
+        docViewer.getAnnotationsLoadedPromise().then(() => {
+          // iterate over fields
+          const fieldManager = annotManager.getFieldManager();
+          fieldManager.forEachField(field => {
+            console.log(field.getValue());
+          });
+        });
+      });
       // now you can access APIs through this.webviewer.getInstance()
-      // instance.openElements(['notesPanel']);
-      // see https://www.pdftron.com/documentation/web/guides/ui/apis for the full list of APIs
 
       // or listen to events from the viewer element
       this.viewer.nativeElement.addEventListener('pageChanged', (e) => {
@@ -65,22 +70,36 @@ export class AppComponent implements OnInit, AfterViewInit {
     // const { xfdf } = response;
   }
 
+  getValues(){
+    const { docViewer, annotManager } = this.wvInstance;
+
+      docViewer.getAnnotationsLoadedPromise().then(() => {
+        // iterate over fields
+        const fieldManager = annotManager.getFieldManager();
+        fieldManager.forEachField(field => {
+          console.log(field.getValue());
+          field.setValue('new value');
+        });
+      });
+
+  }
+
   wvDocumentLoadedHandler(): void {
     // you can access docViewer object for low-level APIs
     const docViewer = this.wvInstance;
     const annotManager = this.wvInstance.annotManager;
     // and access classes defined in the WebViewer iframe
-    const { Annotations } = this.wvInstance;
-    const rectangle = new Annotations.RectangleAnnotation();
-    rectangle.PageNumber = 1;
-    rectangle.X = 100;
-    rectangle.Y = 100;
-    rectangle.Width = 250;
-    rectangle.Height = 250;
-    rectangle.StrokeThickness = 5;
-    rectangle.Author = annotManager.getCurrentUser();
-    annotManager.addAnnotation(rectangle);
-    annotManager.drawAnnotations(rectangle.PageNumber);
+    // const { Annotations } = this.wvInstance;
+    // const rectangle = new Annotations.RectangleAnnotation();
+    // rectangle.PageNumber = 1;
+    // rectangle.X = 100;
+    // rectangle.Y = 100;
+    // rectangle.Width = 250;
+    // rectangle.Height = 250;
+    // rectangle.StrokeThickness = 5;
+    // rectangle.Author = annotManager.getCurrentUser();
+    // annotManager.addAnnotation(rectangle);
+    // annotManager.drawAnnotations(rectangle.PageNumber);
     // see https://www.pdftron.com/api/web/WebViewer.html for the full list of low-level APIs
   }
 }
